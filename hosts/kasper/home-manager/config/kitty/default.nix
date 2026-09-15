92354334136ea6ff8d1e48ac064dbc1fc5787ed8
Kasper Nilssen <kasper.nilssen1@gmail.com>
2026-09-15 10:15:44 +0200

    kitty: add quick-access terminal config (frosted full-screen dropdown)

--- hosts/kasper/home-manager/config/kitty/default.nix
+++ hosts/kasper/home-manager/config/kitty/default.nix
@@ -3,5 +3,6 @@
   xdg.configFile = {
     "kitty/kitty.conf".source = ./kitty.conf;
     "kitty/mocha.conf".source = ./mocha.conf;
+    "kitty/quick-access-terminal.conf".source = ./quick-access-terminal.conf;
   };
 }
